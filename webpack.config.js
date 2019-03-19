3533a31fdb7359cd1a07ff3f36a67299a8bcec35
Kelp <kelp@phate.org>
2019-03-19 12:57:03 +0800

    Install scss loader.

--- webpack.config.js
+++ webpack.config.js
@@ -45,6 +45,20 @@ module.exports = () => {
               }
             }
           ]
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            {
+              loader: 'style-loader'
+            },
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'sass-loader'
+            }
+          ]
         }
       ]
     }
